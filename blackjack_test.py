fab3d8d7b6c2ef86c732b0d6e1e4d6cf5ee25dc5
im <idevm5@ya.ru>
2022-07-31 22:31:50 +0300

    add tests

--- blackjack_test.py
+++ blackjack_test.py
@@ -5,8 +5,33 @@ class BlackjackTests(unittest.TestCase):
     def setUp(self) -> None:
         self.bj = blackjack
 
-    def test(self):
-        pass
+    def test_getBet_ValidInput_return_bet(self):
+        maxBet = 500
+        for i in [1, 5, 10, 200, 500]:
+            with self.subTest(i):
+                self.assertEqual(self.bj.getBet(maxBet, i), i)
+
+    def test_getBet_invalidInput_return_None(self):
+        maxBet = 500
+        for i in [0, -5, 1000, 501]:
+            with self.subTest(i):
+                self.assertIsNone(self.bj.getBet(maxBet, i))
+
+    def test_getDeck_return_listOfTuple(self):
+        self.assertIsInstance(self.bj.getDeck(), list)
+        self.assertIsInstance(self.bj.getDeck()[0], tuple)
+
+    def test_getDeck_return_listOf52Items(self):
+        self.assertEqual(len(self.bj.getDeck()), 52)
+        
+    def test_getHandValue_correctInput_return_value(self):
+        exp = [12, 6]
+        params = [[('2', chr(9829)), ('J', chr(9829))], [('6', chr(9829))]]
+        for i in range(2):
+            with self.subTest(i):
+                self.assertEqual(self.bj.getHandValue(params[i]), exp[i])
+
+        
 
 if __name__ == '__main__':
     unittest.main()
