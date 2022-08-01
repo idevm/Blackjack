cc621179adef84d500d7dcb03aeb9767f9ab06d8
im <idevm5@ya.ru>
2022-08-01 20:52:15 +0300

    correct tests

--- blackjack_test.py
+++ blackjack_test.py
@@ -7,19 +7,17 @@ class BlackjackTests(unittest.TestCase):
     def setUp(self) -> None:
         self.bj = blackjack
 
-    @unittest.skip
     def test_getBet_ValidInput_return_bet(self):
         maxBet = 500
         for i in [1, 5, 10, 200, 500]:
             with self.subTest(i):
-                self.assertEqual(self.bj.getBet(maxBet, i), i)
+                self.assertEqual(self.bj.getBet(maxBet, str(i)), i)
     
-    @unittest.skip
-    def test_getBet_invalidInput_return_None(self):
+    def test_getBet_invalidInput_return_0(self):
         maxBet = 500
-        for i in [0, -5, 1000, 501]:
+        for i in ['g', -5, 1000, 501]:
             with self.subTest(i):
-                self.assertIsNone(self.bj.getBet(maxBet, i))
+                self.assertEqual(self.bj.getBet(maxBet, str(i)), 0)
 
     def test_getDeck_return_listOfTuple(self):
         self.assertIsInstance(self.bj.getDeck(), list)
@@ -34,7 +32,6 @@ class BlackjackTests(unittest.TestCase):
         for i in range(2):
             with self.subTest(i):
                 self.assertEqual(self.bj.getHandValue(params[i]), exp[i])
-
         
 
 if __name__ == '__main__':
